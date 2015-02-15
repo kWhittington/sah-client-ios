8e54bf5ae3904b7b9f2eb019113ada815c086904
Kyle Whittington <kthomaswhittington@gmail.com>
2015-02-15 11:48:27 -0600

    Add Hand and HandController.

---
+++ StringsAgainstHumanity/App/Models/Hand/Hand.h
@@ -0,0 +1,17 @@
+//
+//  Hand.h
+//  StringsAgainstHumanity
+//
+//  Created by Kyle Whittington on 2/15/15.
+//  Copyright (c) 2015 Kyle Whittington. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Hand : NSObject
+
+@property(readonly) NSArray *cards;
+
++ (Hand *)testHand;
+
+@end
---
+++ StringsAgainstHumanity/App/Models/Hand/Hand.m
@@ -0,0 +1,23 @@
+//
+//  Hand.m
+//  StringsAgainstHumanity
+//
+//  Created by Kyle Whittington on 2/15/15.
+//  Copyright (c) 2015 Kyle Whittington. All rights reserved.
+//
+
+#import "Hand.h"
+
+@interface Hand ()
+
+@property NSArray *cards;
+
+@end
+
+@implementation Hand
+
++ (Hand *)testHand {
+  return [[Hand alloc] init];
+}
+
+@end
