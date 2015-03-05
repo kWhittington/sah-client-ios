60999c8657bb7bb2ad135958c364511f0ab11795
Kyle Whittington <kthomaswhittington@gmail.com>
2015-03-05 14:43:41 -0600

    Close #12. Add WhiteCard model, factory, and specs.

---
+++ StringsAgainstHumanity/App/Models/Card/WhiteCard/WhiteCard.h
@@ -0,0 +1,13 @@
+//
+//  WhiteCard.h
+//  StringsAgainstHumanity
+//
+//  Created by Kyle Whittington on 3/5/15.
+//  Copyright (c) 2015 Kyle Whittington. All rights reserved.
+//
+
+#import "Card.h"
+
+@interface WhiteCard : Card
+
+@end
---
+++ StringsAgainstHumanity/App/Models/Card/WhiteCard/WhiteCard.m
@@ -0,0 +1,13 @@
+//
+//  WhiteCard.m
+//  StringsAgainstHumanity
+//
+//  Created by Kyle Whittington on 3/5/15.
+//  Copyright (c) 2015 Kyle Whittington. All rights reserved.
+//
+
+#import "WhiteCard.h"
+
+@implementation WhiteCard
+
+@end
