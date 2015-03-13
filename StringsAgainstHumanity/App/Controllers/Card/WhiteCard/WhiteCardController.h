8b1984dd31bc804f54586cc24dbdacc7239a7dde
Kyle Whittington <kthomaswhittington@gmail.com>
2015-03-13 11:19:40 -0500

    Close #10. Add WhiteCardController.

---
+++ StringsAgainstHumanity/App/Controllers/Card/WhiteCard/WhiteCardController.h
@@ -0,0 +1,20 @@
+//
+//  WhiteCardController.h
+//  StringsAgainstHumanity
+//
+//  Created by Kyle Whittington on 3/13/15.
+//  Copyright (c) 2015 Kyle Whittington. All rights reserved.
+//
+
+@import UIKit;
+
+@class WhiteCard;
+
+@interface WhiteCardController : UIViewController
+@property(readonly, copy, nonatomic) WhiteCard *whiteCard;
+@property(strong, nonatomic) IBOutlet UILabel *label;
+
++ (NSString *)StoryboardID;
+
++ (instancetype)withWhiteCard:(WhiteCard *)whiteCard;
+@end
