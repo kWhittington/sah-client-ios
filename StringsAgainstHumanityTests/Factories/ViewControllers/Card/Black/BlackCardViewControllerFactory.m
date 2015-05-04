f5ad01dc25e13d6575206835cb30db8f863b974f
Kyle Whittington <kthomaswhittington@gmail.com>
2015-05-04 10:34:44 -0500

    Move ViewControllers/Card/BlackCard/ -> ViewControllers/Card/Black/

---
+++ StringsAgainstHumanityTests/Factories/ViewControllers/Card/Black/BlackCardViewControllerFactory.m
@@ -0,0 +1,9 @@
+//
+//  BlackCardViewControllerFactory.m
+//  StringsAgainstHumanity
+//
+//  Created by Kyle Whittington on 5/4/15.
+//  Copyright (c) 2015 Kyle Whittington. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
