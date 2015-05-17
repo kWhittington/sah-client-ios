6aa4b9864375fc4cfc6923abd072942031682279
Kyle Whittington <kthomaswhittington@gmail.com>
2015-05-17 12:18:21 -0500

    Adding top-level documentation to HandView.

--- StringsAgainstHumanity/App/Views/Hand/HandView.h
+++ StringsAgainstHumanity/App/Views/Hand/HandView.h
@@ -5,6 +5,7 @@
 //  Created by Kyle Whittington on 5/10/15.
 //  Copyright (c) 2015 Kyle Whittington. All rights reserved.
 //
+//  Presents cards from a hand to the screen for view, selection, and playing.
 
 @import UIKit;
 
